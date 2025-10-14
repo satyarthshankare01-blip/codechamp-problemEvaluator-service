d89ae48cd3c5b108ffcd81c10b8aac7eb3e03225
satyarthshankare01-blip <satyarthshankare01@gmail.com>
2025-10-14 13:48:23 +0000

    basic redis setup

---
+++ src/config/redisConfig.ts
@@ -0,0 +1,14 @@
+import serverConfig from  "./serverConfig.js" ;
+
+import Redis from "ioredis";
+
+const redisConfig = {
+
+    port : serverConfig.REDIS_PORT,
+    host : serverConfig.REDIS_HOST
+};
+
+const redisConnection = new Redis (redisConfig);
+
+export default redisConnection ;
+
