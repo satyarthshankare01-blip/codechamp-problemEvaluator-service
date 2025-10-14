533d28b8d3db650e04e78466baf8bc12e1605677
satyarthshankare01-blip <satyarthshankare01@gmail.com>
2025-10-14 05:59:09 +0000

    Improved setup and basic server

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+
+
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('TypeScript + Express Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
